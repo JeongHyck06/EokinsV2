6a224152c71dc43ef7f34b98f323adb90c71e57e
Jack <devjack050700@gmail.com>
2024-10-11 12:44:38 +0900

    DB업데이트 항목 추가 (변경)

--- commands/endGame.js
+++ commands/endGame.js
@@ -1,15 +1,14 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-const { teams } = require('../state');
+const { teams, positions, gameStatus } = require('../state');
 const connectDB = require('../database');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('게임종료')
-        .setDescription('승리팀을 결정하고 팀 정보를 업로드합니다.')
+        .setDescription('승리팀을 결정하고 팀 정보를 저장합니다.')
         .addStringOption((option) => option.setName('승리팀').setDescription('승리한 팀을 선택하세요 (1팀 또는 2팀)').setRequired(true).addChoices({ name: '1팀', value: 'team1' }, { name: '2팀', value: 'team2' })),
 
     async execute(interaction) {
-        // Interaction 응답 시간이 길어질 수 있으므로 우선 응답을 지연시킴
         await interaction.deferReply();
 
         const winningTeam = interaction.options.getString('승리팀');
@@ -18,9 +17,10 @@ module.exports = {
 
         const db = await connectDB();
         const playerStats = db.collection('users');
+        const gamesCollection = db.collection('games');
 
-        const updatePlayerStats = async (team, won) => {
-            for (const player of teams[team].players) {
+        const updatePlayerStats = async (won, players) => {
+            for (const player of players) {
                 const member = guild.members.cache.find((m) => m.displayName === player);
                 if (!member) continue;
                 const userId = member.id;
@@ -34,12 +34,36 @@ module.exports = {
             }
         };
 
-        try {
-            await updatePlayerStats(winningTeam, true); // 승리 팀 업데이트
-            await updatePlayerStats(losingTeam, false); // 패배 팀 업데이트
+        const getTeamPlayers = (team) => {
+            if (gameStatus.teamCreationMethod === 'captain') {
+                return teams[team].players;
+            } else if (gameStatus.teamCreationMethod === 'random') {
+                return Object.values(positions[team]).filter((player) => player !== '빈 자리');
+            }
+            return [];
+        };
 
-            // Interaction 완료 후 응답
-            await interaction.followUp('게임 결과가 성공적으로 업데이트되었습니다!');
+        const saveGameResult = async () => {
+            const gameData = {
+                winningTeam: getTeamPlayers(winningTeam),
+                losingTeam: getTeamPlayers(losingTeam),
+                date: new Date(),
+            };
+
+            try {
+                await gamesCollection.insertOne(gameData);
+                console.log('게임 결과가 성공적으로 저장되었습니다.');
+            } catch (error) {
+                console.error('게임 결과 저장 실패:', error);
+            }
+        };
+
+        try {
+            await updatePlayerStats(winningTeam, true, getTeamPlayers(winningTeam)); // 승리 팀 업데이트
+            await updatePlayerStats(losingTeam, false, getTeamPlayers(losingTeam)); // 패배 팀 업데이트
+            await saveGameResult();
+
+            await interaction.followUp('게임 결과가 성공적으로 업데이트되고 저장되었습니다!');
         } catch (error) {
             console.error('데이터베이스 업데이트 실패:', error);
             await interaction.followUp('데이터 업데이트 중 오류가 발생했습니다.');
